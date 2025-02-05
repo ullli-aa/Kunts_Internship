ba2370cd140e2c77d00a003d23b86a44bd341fa9
ullli-aa <juliakunts2005@gmail.com>
2025-02-05 23:36:29 +0300

    add tests

--- GLib/GLib/GLib.cpp
+++ GLib/GLib/GLib.cpp
@@ -1,9 +1,137 @@
-// GLib.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+#include "MyTestMacros.h"
+#include "LibPoint.h"
+#include "LibVector.h"
+
+class Tests {
+public:
+	void LibPointTest_DistanceTo() {
+		LibPoint<double> p1(1.0, 2.0, 3.0);
+		MY_ASSERT_DOUBLE_EQ(p1.DistanceTo(p1), 0.0);
+		MY_ASSERT_DOUBLE_EQ(p1.SquareDistanceTo(p1), 0.0);
+
+		LibPoint<double> p2(3.0, 2.0, 1.0);
+		MY_ASSERT_DOUBLE_EQ(p1.DistanceTo(p2), std::sqrt(8));
+		MY_ASSERT_EQ(p2.SquareDistanceTo(p1), 8);
+
+		p1 = LibPoint<double>(-1.0, -1.0);
+		p2 = LibPoint<double>(1.0, 1.0);
+		MY_ASSERT_DOUBLE_EQ(p2.DistanceTo(p1), std::sqrt(8));
+		MY_ASSERT_EQ(p2.SquareDistanceTo(p1), 8);
+
+		p1 = LibPoint<double>(-2.0, 1.0);
+		p2 = LibPoint<double>(1.0, -2.0);
+		MY_ASSERT_DOUBLE_EQ(p2.DistanceTo(p1), std::sqrt(18));
+		MY_ASSERT_EQ(p2.SquareDistanceTo(p1), 18);
+
+		p1 = LibPoint<double>(std::numeric_limits<double>::max(), std::numeric_limits<double>::max());
+		p2 = LibPoint<double>(-std::numeric_limits<double>::max(), -std::numeric_limits<double>::max());
+		MY_ASSERT_DOUBLE_EQ(p2.DistanceTo(p1), std::sqrt(8) * std::numeric_limits<double>::max());
+	}
+
+	void LibPointTest_Operators()
+	{
+		LibPoint<double> p1(1.5, 2.3, 5.6);
+		MY_ASSERT_TRUE(p1 == p1);
+
+		LibPoint<double> p2(1.5 - 3e-5, 2.3, 5.6);
+		LibPoint<double> p3(1.5 + 1e-5, 2.3, 5.6);
+		MY_ASSERT_TRUE(p1 == p2);
+		MY_ASSERT_FALSE(p1 != p3);
+		MY_ASSERT_FALSE(p2 == p3);
+
+		MY_ASSERT_TRUE(p3.IsEqual(p3));
+		MY_ASSERT_FALSE(p1.IsEqual(p2, 1e-6));
+		MY_ASSERT_TRUE(p2.IsEqual(p3, 1e-3));
+	}
+
+	void LibVectorTest_Parallel()
+	{
+		LibVector<float> vec1(1.1f, 2.2f, 3.3f);
+		LibVector<float> vec2(0.00001f, 0.00002f, 0.00003f);
+		MY_ASSERT_TRUE(vec1.IsParallel(vec2));
+
+		vec2.SetY(1e-5f);
+		MY_ASSERT_FALSE(vec1.IsParallel(vec2, 1e-6));
+
+		vec2.SetX(4).SetY(2e-5f);
+		MY_ASSERT_FALSE(vec2.IsParallel(vec1, 1e-9));
+	}
+
+	void LibVectorTest_IsZero()
+	{
+		LibVector<double> vec(0, 0, 0);
+		MY_ASSERT_TRUE(vec.IsZero());
+
+		vec = LibVector<double>(1e-9, 1e-9, 1e-9);
+		MY_ASSERT_TRUE(vec.IsZero());
+
+		vec = LibVector<double>(1e-5, 1e-9, 1e-9);
+		MY_ASSERT_FALSE(vec.IsZero());
+	}
+
+	void LibVectorTest_ScalarMultOperator()
+	{
+		LibVector<double> vec(1, 2, 3);
+		auto result = vec * 2;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(2, 4, 6));
+
+		result = -1.0 * vec;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(-1, -2, -3));
+
+		result = vec * 0;
+		MY_ASSERT_TRUE(result == LibVector<double>(0, 0, 0));
+
+		result = vec * 1;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(1, 2, 3));
+
+		result = 1e-10 * vec;
+		MY_ASSERT_TRUE(result.IsZero());
+
+		result = vec * 1e10;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(1 * 1e10, 2 * 1e10, 3 * 1e10));
+
+		result = 0.5 * vec;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(0.5, 1, 1.5));
+	}
+
+	void LibVectorTest_ScalarDivOperator()
+	{
+		LibVector<double> vec(1, 2, 3);
+		auto result = vec / 2;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(0.5, 1, 1.5));
+
+		result = vec / -1;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(-1, -2, -3));
+
+		//result = vec / 0;
+
+		result = vec / 1;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(1, 2, 3));
+
+		result = vec / 1e10;
+		MY_ASSERT_TRUE(result.IsZero());
+
+		result = vec / 1e-10;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(1 * 1e10, 2 * 1e10, 3 * 1e10));
+
+		result = vec / 0.5;
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(2, 4, 6.1));
+	}
+
+	void RunAllTests() {
+		RUN_TEST(LibPointTest_DistanceTo);
+		RUN_TEST(LibPointTest_Operators);
+		RUN_TEST(LibVectorTest_Parallel);
+		RUN_TEST(LibVectorTest_IsZero);
+		RUN_TEST(LibVectorTest_ScalarMultOperator);
+		RUN_TEST(LibVectorTest_ScalarDivOperator);
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	Tests test;
+	test.RunAllTests();
 }
--- GLib/GLib/MyTestMacros.h
+++ GLib/GLib/MyTestMacros.h
@@ -1,1 +1,50 @@
 #pragma once
+
+#include <iostream>
+#include <stdexcept>
+#include <cmath>
+
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define RESET "\033[0m"
+
+#define MY_ASSERT_DOUBLE_EQ(expected, actual) \
+    if (std::fabs(expected - actual) > 1e-9) { \
+        std::cerr << RED << "Assertion failed: " << #expected << " == " << #actual \
+                  << " at " << __FILE__ << ":" << __LINE__ << RESET << std::endl; \
+        throw std::runtime_error("Test failed!"); \
+    }
+
+#define MY_ASSERT_EQ(expected, actual) \
+    if ((expected) != (actual)) { \
+        std::cerr << RED << "Assertion failed: " << #expected << " == " << #actual \
+                  << " at " << __FILE__ << ":" << __LINE__ << RESET << std::endl; \
+        throw std::runtime_error("Test failed!"); \
+    }
+
+#define MY_ASSERT_VEC_EQ(expected, actual) \
+    MY_ASSERT_DOUBLE_EQ(expected.X(), actual.X()); \
+    MY_ASSERT_DOUBLE_EQ(expected.Y(), actual.Y()); \
+    MY_ASSERT_DOUBLE_EQ(expected.Z(), actual.Z());
+
+#define MY_ASSERT_TRUE(condition) \
+    if (!(condition)) { \
+        std::cerr << RED << "Assertion failed: " << #condition << " is false but should be true " \
+                  << " at " << __FILE__ << ":" << __LINE__ << RESET << std::endl; \
+        throw std::runtime_error("Test failed!"); \
+    }
+
+#define MY_ASSERT_FALSE(condition) \
+    if (condition) { \
+        std::cerr << RED << "Assertion failed: " << #condition << " is true but should be false " \
+                  << " at " << __FILE__ << ":" << __LINE__ << RESET << std::endl; \
+        throw std::runtime_error("Test failed!"); \
+    }
+
+#define RUN_TEST(test_name) \
+    try { \
+        test_name(); \
+        std::cout << #test_name << GREEN << " OK" << RESET << std::endl; \
+    } catch (const std::runtime_error& e) { \
+        std::cerr << #test_name << RED << " FAILED " << RESET << e.what() << std::endl; \
+    }
