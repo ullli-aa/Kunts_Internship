0d6862e448700d81f12455beda6324a2bc84dc82
ullli-aa <juliakunts2005@gmail.com>
2025-02-06 15:10:09 +0300

    Add more tests for vector

--- GLib/GLib/GLib.cpp
+++ GLib/GLib/GLib.cpp
@@ -6,7 +6,7 @@
 
 class Tests {
 public:
-	void LibPointTest_DistanceTo() {
+	void PointTest_DistanceTo() {
 		LibPoint<double> p1(1.0, 2.0, 3.0);
 		MY_ASSERT_DOUBLE_EQ(p1.DistanceTo(p1), 0.0);
 		MY_ASSERT_DOUBLE_EQ(p1.SquareDistanceTo(p1), 0.0);
@@ -30,7 +30,7 @@ public:
 		MY_ASSERT_DOUBLE_EQ(p2.DistanceTo(p1), std::sqrt(8) * std::numeric_limits<double>::max());
 	}
 
-	void LibPointTest_Operators()
+	void PointTest_Operators()
 	{
 		LibPoint<double> p1(1.5, 2.3, 5.6);
 		MY_ASSERT_TRUE(p1 == p1);
@@ -46,7 +46,7 @@ public:
 		MY_ASSERT_TRUE(p2.IsEqual(p3, 1e-3));
 	}
 
-	void LibVectorTest_Parallel()
+	void VecTest_Parallel()
 	{
 		LibVector<float> vec1(1.1f, 2.2f, 3.3f);
 		LibVector<float> vec2(0.00001f, 0.00002f, 0.00003f);
@@ -59,7 +59,7 @@ public:
 		MY_ASSERT_FALSE(vec2.IsParallel(vec1, 1e-9));
 	}
 
-	void LibVectorTest_IsZero()
+	void VecTest_IsZero()
 	{
 		LibVector<double> vec(0, 0, 0);
 		MY_ASSERT_TRUE(vec.IsZero());
@@ -71,7 +71,24 @@ public:
 		MY_ASSERT_FALSE(vec.IsZero());
 	}
 
-	void LibVectorTest_ScalarMultOperator()
+	void VecTest_EqOperators()
+	{
+		LibVector<double> vec1(1.1, 2.2, 3.3);
+		LibVector<double> vec2(1e-5, 2e-5, 3e-5);
+
+		MY_ASSERT_FALSE(vec1 == vec2);
+
+		vec2.SetY(1e-5);
+		MY_ASSERT_FALSE(vec1.IsEqual(vec2, 1e-6));
+
+		vec2.SetY(2e-5);
+		MY_ASSERT_TRUE(vec1.IsEqual(vec2, 10));
+
+		LibVector<double> vec3(1e-5, 1e-5, 1e-5);
+		MY_ASSERT_TRUE(vec2 != vec3);
+	}
+
+	void VecTest_ScalarMultOperator()
 	{
 		LibVector<double> vec(1, 2, 3);
 		auto result = vec * 2;
@@ -81,7 +98,7 @@ public:
 		MY_ASSERT_VEC_EQ(result, LibVector<double>(-1, -2, -3));
 
 		result = vec * 0;
-		MY_ASSERT_TRUE(result == LibVector<double>(0, 0, 0));
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(0, 0, 0));
 
 		result = vec * 1;
 		MY_ASSERT_VEC_EQ(result, LibVector<double>(1, 2, 3));
@@ -96,7 +113,7 @@ public:
 		MY_ASSERT_VEC_EQ(result, LibVector<double>(0.5, 1, 1.5));
 	}
 
-	void LibVectorTest_ScalarDivOperator()
+	void VecTest_ScalarDivOperator()
 	{
 		LibVector<double> vec(1, 2, 3);
 		auto result = vec / 2;
@@ -117,16 +134,20 @@ public:
 		MY_ASSERT_VEC_EQ(result, LibVector<double>(1 * 1e10, 2 * 1e10, 3 * 1e10));
 
 		result = vec / 0.5;
-		MY_ASSERT_VEC_EQ(result, LibVector<double>(2, 4, 6.1));
+		MY_ASSERT_VEC_EQ(result, LibVector<double>(2, 4, 6));
+
+		result = vec.CrossProduct(LibVector<double>(0, 0, 0));
+		MY_ASSERT_TRUE(result.IsZero());
 	}
 
 	void RunAllTests() {
-		RUN_TEST(LibPointTest_DistanceTo);
-		RUN_TEST(LibPointTest_Operators);
-		RUN_TEST(LibVectorTest_Parallel);
-		RUN_TEST(LibVectorTest_IsZero);
-		RUN_TEST(LibVectorTest_ScalarMultOperator);
-		RUN_TEST(LibVectorTest_ScalarDivOperator);
+		RUN_TEST(PointTest_DistanceTo);
+		RUN_TEST(PointTest_Operators);
+		RUN_TEST(VecTest_Parallel);
+		RUN_TEST(VecTest_IsZero);
+		RUN_TEST(VecTest_EqOperators);
+		RUN_TEST(VecTest_ScalarMultOperator);
+		RUN_TEST(VecTest_ScalarDivOperator);
 	}
 };
 
