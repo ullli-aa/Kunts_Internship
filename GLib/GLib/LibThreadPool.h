ec0f2d0cede6a39170b6704a3f59b8f1658eb9f2
ullli-aa <juliakunts2005@gmail.com>
2025-03-03 08:20:15 +0300

    add some fix in ThreadPool

--- GLib/GLib/LibThreadPool.h
+++ GLib/GLib/LibThreadPool.h
@@ -6,13 +6,18 @@
 
 class Task {
 public:
+	Task() : id(0) {}
 	virtual void Do() = 0;
 	virtual ~Task() = default;
+
+protected:
+	int id;
 };
 
 class LibThreadPool {
 public:
-	LibThreadPool(size_t numThreads = std::thread::hardware_concurrency()) : stop(false) {
+	LibThreadPool(size_t numThreads = std::thread::hardware_concurrency()) : 
+		stop(false), LastId(0), complete(0) {
 		for (size_t i = 0; i < numThreads; i++)
 		{
 			threads.emplace_back([this] {
@@ -20,15 +25,21 @@ public:
 					std::unique_ptr<Task> task;
 					
 					std::unique_lock<std::mutex> lock(queueMutex);
-					condition.wait(lock, [this] { return stop || !tasks.empty(); });
+					cv_add.wait(lock, [this] { return stop || !tasks.empty(); });
+
+					if (stop) {
+						return;
+					}
 
 					task = std::move(tasks.front());
 					tasks.pop();
 
 					lock.unlock();
-					
+
 					task->Do();
-					condition.notify_all();
+					complete++;
+
+					cv_wait.notify_all();
 				}
 			});
 		}
@@ -40,37 +51,39 @@ public:
 
 	void AddTask(std::unique_ptr<Task> task) {
 		std::unique_lock<std::mutex> lock(queueMutex);
+
+		LastId++;
 		tasks.push(std::move(task));
 		lock.unlock();
 		
-		condition.notify_one();
+		cv_add.notify_one();
 	}
 
 	void WaitForFinish() {
-		std::unique_lock<std::mutex> lock(queueMutex);
-		condition.wait(lock, [this] { return tasks.empty() || stop; });
+		std::unique_lock<std::mutex> lock(waitMutex);
+		cv_wait.wait(lock, [this] { return tasks.empty() && complete == LastId; });
 	}
 
 	void StopPool() {
 		stop = true;
 
-		condition.notify_all();
+		cv_add.notify_all();
 		for (std::thread& thread : threads) {
 			thread.join();
 		}
-	}
-
-	size_t NumThreads() const {
-		return threads.size();
 	}
 
 private:
 	std::vector<std::thread> threads;
 	std::queue<std::unique_ptr<Task>> tasks;
 	std::mutex queueMutex;
-	std::condition_variable condition;
-	std::atomic<bool> stop;
-};
+	std::condition_variable cv_add;
 
-//у таска айди и считать колво обработанных и если обр = ласт инд то финиш
-// заводить разные кондитионал вэрибл
+	std::mutex waitMutex;
+	std::condition_variable cv_wait;
+
+	int LastId;
+	std::atomic<bool> stop;
+	std::atomic<int> complete;
+
+};
